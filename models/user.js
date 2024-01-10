353a464e54a802621e26754f8cb40b6191714c88
RSwetha02 <156092670+RSwetha02@users.noreply.github.com>
2024-01-10 14:18:58 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,15 @@
-
+const mongoose=require('mongoose');
+const userSchema=new mongoose.Schema({
+    rno:Number,
+    Student_Name:String,
+            Father_Name:String,
+            Mother_Name:String,
+            DOB:String,
+            Email:String,
+            Phone_No:Number,
+            Gender:String,
+            Level:String,
+            Department:String,
+},{versionKey:false});
+const User=mongoose.model('student_record',userSchema);
+module.exports=User;
